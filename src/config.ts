0a5b63ac1b33cde97ee8e130e390cac19041487a
gitea <admin@admin.com>
2025-03-20 17:16:21 +0800

    init

--- src/config.ts
+++ src/config.ts
@@ -1,39 +1,33 @@
 export default {
-  Title: '韩小韩博客',
+  Title: '崔晃晃',
   // 访问的域名
-  Site: 'https://www.vvhan.com',
-  Subtitle: '不曾与你分享的时间,我在进步.',
-  Description: '韩小韩博客 专注于前开发与相关技术的实战分享，涵盖Vue框架、Node.js、Serverless等，并涉及Node、Python、Linux、Docker等领域。同时，博客也分享作者的生活、音乐和旅行的热爱。',
-  Author: '.𝙃𝙖𝙣',
-  Motto: '运气是计划之外的东西.',
-  Avatar: 'https://q1.qlogo.cn/g?b=qq&nk=1655466387&s=640',
+  Site: 'https://cuihuanghuang.com',
+  Subtitle: '让赚钱变得更简单',
+  Description: '崔晃晃博客 专注国外网赚',
+  Author: '崔晃晃',
+  Motto: '选择大于努力',
+  Avatar: 'https://cdn.usd.fan/user_files/88/avatar/48882808_1654931162.jpeg',
   // 导航栏 (新窗口打开 newWindow: true)
   Navs: [
     { text: '朋友', link: '/links', icon: '/assets/images/svg/Navs/links.svg' },
     { text: '圈子', link: '/friends', icon: '/assets/images/svg/Navs/friends.svg' },
     { text: '动态', link: '/talking', icon: '/assets/images/svg/Navs/talking.svg' },
-    { text: '昔日', link: '/archives', icon: '/assets/images/svg/Navs/archives.svg' },
+    // { text: '昔日', link: '/archives', icon: '/assets/images/svg/Navs/archives.svg' },
     { text: '留言', link: '/message', icon: '/assets/images/svg/Navs/message.svg' },
     { text: '关于', link: '/about', icon: '/assets/images/svg/Navs/about.svg' },
-    { text: 'API', link: 'https://api.vvhan.com/', target: true, icon: '/assets/images/svg/Navs/link.svg' },
+    { text: '讨论', link: 'https://usd.fan/', target: true, icon: '/assets/images/svg/Navs/link.svg' },
   ],
   // 侧边栏个人网站
   WebSites: [
-    { text: 'Github', link: 'https://github.com/uxiaohan', icon: '/assets/images/svg/WebSites/github.svg' },
-    { text: '韩小韩API', link: 'https://api.vvhan.com', icon: '/assets/images/svg/WebSites/api.svg' },
-    { text: '每日热榜', link: 'https://hot.vvhan.com', icon: '/assets/images/svg/WebSites/hot.svg' },
-    { text: '骤雨重山图床', link: 'https://wp-cdn.4ce.cn', icon: '/assets/images/svg/WebSites/img.svg' },
-    { text: 'HanAnalytics', link: 'https://analytics.vvhan.com', icon: '/assets/images/svg/WebSites/analytics.svg' },
+    { text: 'usd.fan', link: 'https://usd.fan', icon: '/assets/images/svg/WebSites/hot.svg' },
   ],
   // Cover 使用绝对路径
   Cover: '/assets/images/banner/072c12ec85d2d3b5.webp',
-  CreateTime: '2021-09-01',
+  CreateTime: '2016-11-15',
   // DNS预解析地址
   DNSOptimization: [
     'https://i0.wp.com',
-    'https://tools-api.vvhan.com',
     'https://pagead2.googlesyndication.com',
-    'https://lf9-cdn-tos.bytecdntp.com',
     'https://registry.npmmirror.com',
     'https://npm.elemecdn.com'
   ],
@@ -47,8 +41,8 @@ export default {
   GoogleAds: {
     ad_Client: 'ca-pub-xxxxxx',
     // 侧边栏广告(不填不开启)
-    asideAD_Slot: `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-xxxxxx" data-ad-slot="xxxxxx" data-ad-format="auto" data-full-width-responsive="true"></ins>`,
+    asideAD_Slot: ``,
     // 文章页广告(不填不开启)
-    articleAD_Slot: `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-xxxxxx" data-ad-slot="xxxxxx" data-ad-format="auto" data-full-width-responsive="true"></ins>`
+    articleAD_Slot: ``
   }
 }
